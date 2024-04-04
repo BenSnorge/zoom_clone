bbe4e6a2e0bbbaa4062ffac78e310bd97b0a744d
Benjamin Sparwasser <benjaminsparwasser@gmail.com>
2024-04-04 15:09:25 +0200

    batman3

--- hooks/userGetCallById.ts
+++ hooks/userGetCallById.ts
@@ -1,5 +1,31 @@
-import { useState } from "react"
+import { useEffect, useState } from 'react';
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 
 export const useGetCallById = (id: string | string[]) => {
-    const [call, setCall] = useState<CallTrackerCall>()
-} 
+    const [call, setCall] = useState<Call>();
+    const [isCallLoading, setIsCallLoading] = useState(true);
+
+    const client = useStreamVideoClient();
+
+    useEffect(() => {
+        if (!client) return;
+
+        const loadCall = async () => {
+            try {
+                // https://getstream.io/video/docs/react/guides/querying-calls/#filters
+                const { calls } = await client.queryCalls({ filter_conditions: { id } });
+
+                if (calls.length > 0) setCall(calls[0]);
+
+                setIsCallLoading(false);
+            } catch (error) {
+                console.error(error);
+                setIsCallLoading(false);
+            }
+        };
+
+        loadCall();
+    }, [client, id]);
+
+    return { call, isCallLoading };
+};
